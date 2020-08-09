11680a6fdbe665e6b6e7708c265a769d7766578f
bill <bdavisx@yahoo.com>
2020-08-09 11:32:35 -0500

    DirectCallVerticle working, tests successful. Need to convert existing verticles where it makes sense.

---
+++ src/main/kotlin/com/tartner/vertx/DirectCallVerticle.kt
@@ -0,0 +1,80 @@
+package com.tartner.vertx
+
+import io.vertx.core.eventbus.DeliveryOptions
+import io.vertx.core.eventbus.Message
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.launch
+
+sealed class CodeMessage<T: Any>(val block: suspend (DirectCallVerticle) -> T)
+class ReturnValueCodeMessage<T: Any>(block: suspend (DirectCallVerticle) -> T): CodeMessage<T>(block)
+class UnitCodeMessage(block: suspend (DirectCallVerticle) -> Unit): CodeMessage<Unit>(block)
+class FireAndForgetCodeMessage(block: suspend (DirectCallVerticle) -> Unit): CodeMessage<Unit>(block)
+
+/**
+ * Use the same id for multiple verticles if you want the calls to be distributed.
+ */
+open class DirectCallVerticle(val localAddress: String): CoroutineVerticle() {
+
+  companion object {
+    val codeDeliveryOptions = DeliveryOptions()
+    init { codeDeliveryOptions.codecName = CodeMessage::class.qualifiedName }
+
+    fun isDirectCallVerticle(jvmType: Class<*>) =
+      DirectCallVerticle::class.java.isAssignableFrom(jvmType)
+  }
+
+  override suspend fun start() {
+    super.start()
+    vertx.eventBus().localConsumer<CodeMessage<*>>(localAddress,
+      { launch(vertx.dispatcher()) { runCode(it) } })
+  }
+
+  /**
+   * Code inside `block` will run on the event loop for this (set of) verticle(s). The code runs
+   * like coroutines are expected to run, the calling thread awaits on the return, it does not "fire
+   * and forget".
+   */
+  protected suspend fun act(block: suspend (DirectCallVerticle) -> Unit) {
+    // we could fire and forget here, but we want the semantics of "imperative" code like coroutines have
+    awaitMessageResult<Any> {
+      eventBus.request(localAddress, UnitCodeMessage(block), codeDeliveryOptions, it)
+    }
+  }
+
+  /**
+   * Code inside `block` will run on the event loop for this (set of) verticle(s). The code runs
+   * like coroutines are expected to run, the calling thread awaits on the return, it does not "fire
+   * and forget". The value returned by the block will be the return value for this function.
+   */
+  protected suspend fun <T: Any> actAndReply(block: suspend (DirectCallVerticle) -> T): T {
+    return awaitMessageResult {
+      eventBus.request(localAddress, ReturnValueCodeMessage(block), codeDeliveryOptions, it)
+    }
+  }
+
+  /**
+   * Code inside `block` will run on the event loop for this (set of) verticle(s). The calling
+   * thread returns immediately.
+   */
+  protected fun fireAndForget(block: suspend (DirectCallVerticle) -> Unit) {
+    eventBus.send(localAddress, FireAndForgetCodeMessage(block), codeDeliveryOptions)
+  }
+
+  private suspend fun runCode(codeMessage: Message<CodeMessage<*>>) {
+      // TODO: exceptions?
+    val code = codeMessage.body()!!
+    when (codeMessage.body()) {
+      is UnitCodeMessage -> {
+        code.block.invoke(this)
+        codeMessage.reply(1)
+      }
+      is FireAndForgetCodeMessage -> {
+        code.block.invoke(this)
+      }
+      is ReturnValueCodeMessage -> {
+        codeMessage.reply(code.block.invoke(this))
+      }
+    }
+  }
+}
