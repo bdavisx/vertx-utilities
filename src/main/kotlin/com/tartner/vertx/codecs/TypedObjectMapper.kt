628542a9d1572ef7503f224c61f22c977319497d
bill <bdavisx@yahoo.com>
2020-08-14 18:59:19 -0500

    Removed some deprecated jackson config

--- src/main/kotlin/com/tartner/vertx/codecs/TypedObjectMapper.kt
+++ src/main/kotlin/com/tartner/vertx/codecs/TypedObjectMapper.kt
@@ -23,6 +23,7 @@ import arrow.core.Option
 import arrow.core.Some
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
@@ -34,7 +35,7 @@ import com.tartner.utilities.jackson.JacksonArrowOptionSomeDeserializer
 
 /**
 Note: This class should only be used for "internal"/trusted serialization/deserialization. There
-are security issues around Jackson Datamapper and `enableDefaultTyping...` is part of what allows
+are security issues around Jackson Datamapper and `activateDefaultTyping...` is part of what allows
 the exploit to happen. As long as the json is trusted, this class is safe to use. So there needs to
 be another class that is used for stuff coming in over the internet.
  */
@@ -46,7 +47,9 @@ class TypedObjectMapper: ObjectMapper() {
   init {
     registerKotlinModule()
     registerModule(JavaTimeModule())
-    enableDefaultTyping(DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
+//    enableDefaultTyping(DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY)
+    activateDefaultTyping(LaissezFaireSubTypeValidator(), DefaultTyping.NON_FINAL,
+      JsonTypeInfo.As.PROPERTY)
     addMixIn(Either::class.java, ArrowMixIn::class.java)
     addMixIn(Option::class.java, ArrowMixIn::class.java)
 
